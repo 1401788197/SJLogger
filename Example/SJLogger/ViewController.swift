53f063eb8b26397bf12d14e7e206268cbffa1997
Hicreate <shengjie@hai.group>
2026-01-05 11:43:38 +0800

    Initial commit

---
+++ Example/SJLogger/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SJLogger
+//
+//  Created by Hicreate on 01/05/2026.
+//  Copyright (c) 2026 Hicreate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
